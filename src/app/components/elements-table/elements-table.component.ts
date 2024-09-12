0b7a29602495ac86f499c1c8b3c3fefc320e4171
Darek Sańczyk <tantyk@gmail.com>
2024-09-12 15:54:01 +0200

    making some structure

---
+++ src/app/components/elements-table/elements-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-elements-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './elements-table.component.html',
+  styleUrl: './elements-table.component.scss'
+})
+export class ElementsTableComponent {
+
+}
