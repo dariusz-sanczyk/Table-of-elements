6b0b9d00cce0ea482938ed4c2b085566dc387d08
Darek Sańczyk <tantyk@gmail.com>
2024-09-12 17:40:18 +0200

    displaying data

--- src/app/components/elements-table/elements-table.component.ts
+++ src/app/components/elements-table/elements-table.component.ts
@@ -1,25 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { ElementsService } from '../../services/elements.service';
 import { PeriodicElement } from '../../models/element.model';
+import { MatTableModule } from '@angular/material/table';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-elements-table',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, NgFor],
   templateUrl: './elements-table.component.html',
   styleUrl: './elements-table.component.scss'
 })
 export class ElementsTableComponent implements OnInit {
   elementsData: PeriodicElement[] = [];
+  columnsToDisplay: string[] = ['position', 'name', 'weight', 'symbol'];
+  headersToDisplay: string[] = ['Number', 'Name', 'Weight', 'Symbol'];
 
-  constructor(private elementsService: ElementsService) {};
+  constructor(private elementsService: ElementsService) {
+
+  };
 
   ngOnInit(): void {
     this.getElements();
     console.log(this.elementsData)
   }
 
-  getElements() {
+  getElements(): void {
     this.elementsData = this.elementsService.getElements();
+  }
+
+  getColumnName(column: string): string {
+    switch (column) {
+      case 'position': return 'Number';
+      case 'name': return 'Name';
+      case 'weight': return 'Weight';
+      case 'symbol': return 'Symbol';
+      default: return "";
+    }
   }
 }
