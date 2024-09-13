30ebcd03a262028894ef49be63ad2716e72e33dd
Darek Sańczyk <tantyk@gmail.com>
2024-09-13 16:15:26 +0200

    delay for filtering

--- src/app/components/elements-table/elements-table.component.ts
+++ src/app/components/elements-table/elements-table.component.ts
@@ -5,27 +5,29 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { NgFor } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-elements-table',
   standalone: true,
-  imports: [MatTableModule, MatInputModule, MatFormFieldModule, NgFor],
+  imports: [MatTableModule, MatInputModule, MatFormFieldModule, ReactiveFormsModule, NgFor],
   templateUrl: './elements-table.component.html',
   styleUrl: './elements-table.component.scss'
 })
 export class ElementsTableComponent {
-  elementsData: PeriodicElement[] = [];
-  dataSource: MatTableDataSource<PeriodicElement>;
-  columnsToDisplay: string[] = ['position', 'name', 'weight', 'symbol'];
+  public dataSource: MatTableDataSource<PeriodicElement>;
+  public columnsToDisplay: string[] = ['position', 'name', 'weight', 'symbol'];
+  public inputControl = new FormControl();
 
   constructor(private elementsService: ElementsService) {
     this.dataSource = new MatTableDataSource(this.elementsService.getElements());
+    this.inputControl.valueChanges
+      .pipe(debounceTime(2000))
+      .subscribe((value: string) => {
+        this.applyFilter(value);
+      });
   };
-
-
-  getElements(): void {
-    this.elementsData = this.elementsService.getElements();
-  }
 
   getColumnName(column: string): string {
     switch (column) {
@@ -35,10 +37,9 @@ export class ElementsTableComponent {
       case 'symbol': return 'Symbol';
       default: return "";
     }
-  }
+  };
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  applyFilter(value: string) {
+    this.dataSource.filter = value.trim().toLowerCase();
   }
-}
+};
